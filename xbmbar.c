8b85659f0477adcc0ffdffb8a0a183c4867d904e
Nauxuron <nauxuron@wimdewith.com>
2014-12-10 23:33:50 +0100

    Clean code and add error message

--- xbmbar.c
+++ xbmbar.c
@@ -2,64 +2,81 @@
 #include <stdlib.h>
 #include <getopt.h>
 
-void parse_percentage(unsigned char *result, int percentage) {
+void parse_percentage(unsigned char *result, const int percentage) {
 	int temp_per = percentage + 2;
         int counter = 0;
+
 	while (temp_per >= 8) {
 		result[counter] |= 0xff;
 		temp_per -= 8;
                 counter++;
 	}
+
         result[counter] |= (1 << temp_per) - 1;
         result[12] |= 0xc0;
-            
 }
 
 int main(int argc, char *argv[]) {
 	char opt;
-	int width, height, percentage;
+	int percentage;
+        int perc_flag = 0;
 	while ((opt = getopt(argc, argv, "hp:")) != -1) {
 		switch (opt) {
 			case 'h':
 				printf("xbmbar [-h] [-x WIDTH] [-h HEIGHT] [-p PERCENTAGE]\n");
 				exit(0);
-			case 'x':
-				width = atoi(optarg);
-				break;
-			case 'y':
-				height = atoi(optarg);
-				break;
 			case 'p':
 				percentage = atoi(optarg);
+                                perc_flag = 1;
 				break;
 		}
 	}
-	printf("%s\n", "#define xbmbar_width 104");
-	printf("%s\n", "#define xbmbar_height 8");
+        if (perc_flag != 1) {
+            fprintf(stderr, "%s: %s", argv[0], "Try 'xbmbar -h' for more information.\n");
+            exit(1);
+        }
+
+        int width = 104;
+        int height = 8;
+
+	printf("%s %d\n", "#define xbmbar_width", width);
+	printf("%s %d\n", "#define xbmbar_height", height);
 	printf("%s\n", "static unsigned char xbmbar_bits[] = {");
 
-	for (int i = 0; i < 2; i++) {
-		printf("\t%s\n", "0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,");
+	for (int i = 0; i < height/4; i++) {
+                printf("\t");
+                for (int j = 0; j < width/8; j++) {
+                    printf("%s, ", "0xff");
+                }
+                printf("\n");
 	}
         
-        int bar_size = 13 * 8;
-	unsigned char *bar = calloc(1, bar_size);
+	unsigned char *bar = calloc(1, width);
 	parse_percentage(bar, percentage);
 
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < height/2; i++) {
                 printf("\t");
-		for (int j = 0; j < 13; j++) {
-			printf("0x%x, ", bar[j]);
+		for (int j = 0; j < width/8; j++) {
+			printf("0x%02x, ", bar[j]);
 		}
 		printf("\n");
 	}
     
         free(bar);
 
-	printf("\t%s\n", "0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,");
-	printf("\t%s\n", "0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff");
-
-	printf("%s\n", "};");
+        for (int i = 0; i < (height/4 - 1); i++) {
+                printf("\t");
+                for (int j = 0; j < width/8; j++) {
+                    printf("%s, ", "0xff");
+                }
+                printf("\n");
+	}
+        printf("\t");
+        for (int j = 0; j < (width/8 - 1); j++) {
+                printf("%s, ", "0xff");
+        }
+        printf("0xff");
+	printf(" %s\n", "};");
 
 	return 0;
 }
